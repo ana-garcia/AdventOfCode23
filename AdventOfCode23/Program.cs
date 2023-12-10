437c6d1c4c044d4bfe99741f67cb29bde2fc85d7
Ana Garcia <garcia.pipa.37@gmail.com>
2023-12-10 19:15:49 +0000

    program.cs improvements

--- AdventOfCode23/Program.cs
+++ AdventOfCode23/Program.cs
@@ -1,11 +1,15 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using AdventOfCode23.Solutions;
 
-
+var path = @"C:\Users\Ana Garcia\Documents\Visual Studio 2022\AdventOfCode23\AdventOfCode23\Inputs";
+/*
 var day1 = new Day1();
 
-int solution1 = day1.GetSolution(@"C:\Users\Ana Garcia\Documents\Visual Studio 2022\AdventOfCode23\AdventOfCode23\Inputs\Day1TrebuchetInput.txt", true);
+int solution1 = day1.GetSolution(Path.Combine(path, "Day1TrebuchetInput.txt") , true);
 Console.WriteLine("Day1, Part1: " + solution1);
 
-int solution2 = day1.GetSolution(@"C:\Users\Ana Garcia\Documents\Visual Studio 2022\AdventOfCode23\AdventOfCode23\Inputs\Day1TrebuchetInput.txt", false);
+int solution2 = day1.GetSolution(Path.Combine(path, "Day1TrebuchetInput.txt") , false);
 Console.WriteLine("Day1, Part2: " + solution2);
+*/
+
+var day2 = new Day2();
